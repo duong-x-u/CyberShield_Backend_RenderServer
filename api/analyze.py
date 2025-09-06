06021702e32beda4fb8f776ae8315469f9e832d6
Duong.Dev <139242756+duong-x-u@users.noreply.github.com>
2025-09-06 20:37:27 +0700

    Update analyze.py

--- api/analyze.py
+++ api/analyze.py
@@ -114,7 +114,7 @@ async def analyze_with_gemini(text, keywords_str):
         try:
             selected_api_key = random.choice(GOOGLE_API_KEYS)
             genai.configure(api_key=selected_api_key)
-            model = genai.GenerativeModel("gemini-1.5-pro-latest")
+            model = genai.GenerativeModel("gemini-1.5-flash-latest")
             prompt = UNIFIED_PROMPT(text, keywords_str)
             response = await model.generate_content_async(prompt)
             json_text = response.text.replace("```json", "").replace("```", "").strip()
